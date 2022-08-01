748ca80c7d79ef7f9b04948564e7b7c68e9fe9e3
void_lhf <void_lhf@qq.com>
2022-08-02 00:49:01 +0800

    0

--- js/alixiaozhan.js
+++ js/alixiaozhan.js
@@ -18,7 +18,7 @@
     setInterval(() => {
         try {
             var selector2 = "ul.DiscussionList-discussions>li,div.davwheat-ad.davwheat-ad-header";
-            var includeText2 = ["关于标题党问题公告", "由于小站经常被人恶意cc攻击", "求助帖不发到【求助】板块", "小站防失联平台【小站订阅系统】","软件游戏破解|尺度更大更开放的搞机平台"];
+            var includeText2 = ["关于标题党问题公告", "由于小站经常被人恶意cc攻击", "求助帖不发到【求助】板块", "小站防失联平台【小站订阅系统】", "软件游戏破解|尺度更大更开放的搞机平台"];
             var elements2 = document.querySelectorAll(selector2);
             for (var i1 = 0; i1 < elements2.length; i1++) {
                 var tempStr1 = elements2[i1].innerText;
@@ -30,11 +30,20 @@
                 }
             }
         } catch (err) { }
+
+        try {
+            document.querySelector("div.btn-close--2vCRo").click();
+        } catch (error) { }
+
+        try {
+            document.querySelector("div.download-app--21E6F").remove();
+        } catch (error) { }
+
     }, 100);
 
     /*改变主题*/
     try {
-        var insertStyle = document.createElement('style'); insertStyle.innerHTML = ".drawer-backdrop,.dropdown-backdrop{background:black;} .Modal-body{background:white;} .modal-backdrop{background:none;} .open .dropdown-backdrop,.drawer-backdrop.in{opacity:0.7;}";
+        var insertStyle = document.createElement('style'); insertStyle.innerHTML = ".drawer-backdrop,.dropdown-backdrop{background:black;} .Modal-body{background:white;} .modal-backdrop{background:none;} .open .dropdown-backdrop,.drawer-backdrop.in{opacity:0.7;} .banner-wrap--1xSCA {-webkit-box-shadow:none !important;box-shadow:none !important;}";
         document.head.appendChild(insertStyle);
     } catch (err) { }
 })();
