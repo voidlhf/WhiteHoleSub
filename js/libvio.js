c81a5c04002e8a81d6d833c1f5a0bb65d195326e
void_lhf <void_lhf@qq.com>
2022-08-01 12:17:21 +0800

    0

--- js/libvio.js
+++ js/libvio.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         LIBVIO
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.libvio.me/*
-// @icon         https://www.google.com/s2/favicons?domain=libvio.com
-// @grant        none
-// ==/UserScript==
-
 (function () {
 
     /*设置标题*/
