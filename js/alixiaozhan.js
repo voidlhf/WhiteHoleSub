1f6855aa4187781a3f275af863a25df9ccf13d8c
void_lhf <void_lhf@qq.com>
2022-08-01 21:01:32 +0800

    0

--- js/alixiaozhan.js
+++ js/alixiaozhan.js
@@ -7,7 +7,7 @@
 
     /*屏蔽元素*/
     try {
-        var selector = "div.footer,div#header-primary,div.davwheat-ad.davwheat-ad-header";
+        var selector = "div.footer,div#header-primary";
         var elements = document.querySelectorAll(selector);
         for (var i = 0; i < elements.length; i++) {
             elements[i].style.cssText = "display:none !important";
@@ -17,8 +17,8 @@
     /*屏蔽包含某字符串的元素*/
     setInterval(() => {
         try {
-            var selector2 = "ul.DiscussionList-discussions>li";
-            var includeText2 = ["关于标题党问题公告", "由于小站经常被人恶意cc攻击", "求助帖不发到【求助】板块", "小站防失联平台【小站订阅系统】"];
+            var selector2 = "ul.DiscussionList-discussions>li,div.davwheat-ad.davwheat-ad-header";
+            var includeText2 = ["关于标题党问题公告", "由于小站经常被人恶意cc攻击", "求助帖不发到【求助】板块", "小站防失联平台【小站订阅系统】","软件游戏破解|尺度更大更开放的搞机平台"];
             var elements2 = document.querySelectorAll(selector2);
             for (var i1 = 0; i1 < elements2.length; i1++) {
                 var tempStr1 = elements2[i1].innerText;
