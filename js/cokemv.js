cb88a8812a113eb9ad3d7f72bcab58ae44e17cdd
void_lhf <void_lhf@qq.com>
2022-10-16 01:34:36 +0800

    0

--- js/cokemv.js
+++ js/cokemv.js
@@ -45,9 +45,9 @@
 	} catch (err) { }*/
 
 	/*自动关闭通知弹窗*/
-	try {
+	/*try {
 		document.querySelector("div.popup-btn.close-pop").click();
-	} catch (err) { }
+	} catch (err) { }*/
 
 	try {
 		document.querySelector("body > script:nth-child(5)").nextSibling.remove();
