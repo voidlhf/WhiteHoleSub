975817e68b94b1fb487dcea0367f174c83d434ad
void_lhf <void_lhf@qq.com>
2023-02-04 14:10:14 +0800

    0

--- whiteholesearcher/js/alixiaozhan.js
+++ whiteholesearcher/js/alixiaozhan.js
@@ -7,7 +7,7 @@
 
     /*屏蔽元素*/
     try {
-        var selector = "div.footer,div#header-primary";
+        var selector = "div.footer,div#header-primary,div#header-secondary>ul.Header-controls>li.item-nightmode";
         var elements = document.querySelectorAll(selector);
         for (var i = 0; i < elements.length; i++) {
             elements[i].style.cssText = "display:none !important";
