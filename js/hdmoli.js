c7f1fd18e7c16921830b835414c434e133658c29
void_lhf <void_lhf@qq.com>
2023-06-15 07:47:00 +0800

    0

--- js/hdmoli.js
+++ js/hdmoli.js
@@ -92,52 +92,52 @@
       document.querySelector("body > div.ldgb.ldg").remove();
     } catch (error) { }
   }, 100);
-  
-  if(location.href.startsWith("https://www.cupfox.app/")) {
-		(function () {
-    /*设置标题*/
-    /*try {
-      document.title = vod_name + vod_part;
-    } catch (err) { }*/
 
-    /*屏蔽元素*/
-    try {
+  if (location.href.startsWith("https://www.cupfox.app/")) {
+    (function () {
+      /*设置标题*/
+      /*try {
+        document.title = vod_name + vod_part;
+      } catch (err) { }*/
+
+      /*屏蔽元素*/
+      try {
         var selector = "div.mobile-nav,div.banner,div.bm-menu-wrap > div.bm-menu > nav > div > div:nth-child(2),div.banner-box,div.mobile-container > div > div.cupfox-box,div.mobile-container > div > div.card.border-shadow > div > div.cupfox-box,div.tab-bar";
         var elements = document.querySelectorAll(selector);
         for (var i = 0; i < elements.length; i++) {
-            elements[i].style.cssText = "display:none !important";
+          elements[i].style.cssText = "display:none !important";
         }
-    } catch (err) { }
+      } catch (err) { }
 
-    try {
+      try {
         removeElements("div.card.border-shadow", ["茶杯狐最新地址", "微信关注", "相关推荐"]);
-    } catch (err) { }
+      } catch (err) { }
 
-    /*禁止复制脏东西到剪贴板 */
-    try {
+      /*禁止复制脏东西到剪贴板 */
+      try {
         localStorage.setItem("copySign", "{\"sign\":\"1\",\"time\":1800000000000}");
-    } catch (error) { }
+      } catch (error) { }
 
-    /*禁止弹窗 */
-    try {
+      /*禁止弹窗 */
+      try {
         localStorage.setItem("downloadAppModel", "{\"sign\":\"1\",\"time\":1800000000000}");
-    } catch (error) { }
+      } catch (error) { }
 
-    /*自动关闭通知弹窗*/
-    try {
+      /*自动关闭通知弹窗*/
+      try {
         document.querySelector("span.icon-close").click();
-    } catch (err) { }
+      } catch (err) { }
 
 
-    setInterval(() => {
+      setInterval(() => {
         try {
-            removeElements("div.card.border-shadow", ["相关推荐", "搜索结果来自"]);
+          removeElements("div.card.border-shadow", ["相关推荐", "搜索结果来自"]);
         } catch (err) { }
 
         /*try {
             removeElements("div.tab-bar", ["在线播放"]);
         } catch (err) { }*/
-    }, 100);
-})();
-	}
+      }, 100);
+    })();
+  }
 })();
